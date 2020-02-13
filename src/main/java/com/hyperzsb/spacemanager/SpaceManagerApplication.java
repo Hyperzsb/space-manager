265756119ea09c02dfff7ddb1b1bd0c1f41cb0d4
Overloadlightning <1948765057@qq.com>
2020-02-13 11:08:42 +0800

    [feat]Unable Spring Security

--- src/main/java/com/hyperzsb/spacemanager/SpaceManagerApplication.java
+++ src/main/java/com/hyperzsb/spacemanager/SpaceManagerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class SpaceManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpaceManagerApplication.class, args);
